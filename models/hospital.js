c22a11da0e4ade3e880359c0077ea86e91efc1f9
julianmclain <julianrmclain@gmail.com>
2020-08-12 20:27:54 -0700

    Add associations for Hospital and HospitalStatusUpdate

--- models/hospital.js
+++ models/hospital.js
@@ -1,17 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Hospital extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define associations here
-      // TODO
-    }
-  }
+  class Hospital extends Model {}
   Hospital.init(
     {
       id: {
--- models/hospitalStatusUpdate.js
+++ models/hospitalStatusUpdate.js
@@ -1,5 +1,8 @@
 "use strict";
 const { Model } = require("sequelize");
+const Hospital = require("./hospital");
+const hospitalAdministrator = require("./hospitalAdministrator");
+
 module.exports = (sequelize, DataTypes) => {
   class HospitalStatusUpdate extends Model {
     /**
@@ -8,8 +11,22 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      // TODO
+      HospitalStatusUpdate.belongsTo(Hospital, {
+        foreignKey: {
+          name: "hospitalId",
+          type: DataTypes.UUID,
+          unique: true,
+          allowNull: false,
+        },
+      });
+      HospitalStatusUpdate.belongsTo(hospitalAdministrator, {
+        foreignKey: {
+          name: "hospitalAdministratorId",
+          type: DataTypes.UUID,
+          unique: true,
+          allowNull: false,
+        },
+      });
     }
   }
   HospitalStatusUpdate.init(
