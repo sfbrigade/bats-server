6c90905b575043364b39b6c054fcbd8cd13111b2
julianmclain <julianrmclain@gmail.com>
2020-08-15 10:50:16 -0700

    Re-order drop table sequence

--- test/helper.js
+++ test/helper.js
@@ -1,41 +1,41 @@
-'use strict'
+"use strict";
 
 /// MUST BE FIRST! set the NODE_ENV to test to disable logging, switch to test db
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-const fixtures = require('sequelize-fixtures');
-const path = require('path');
+const fixtures = require("sequelize-fixtures");
+const path = require("path");
 
-const models = require('../models');
+const models = require("../models");
 
-const loadFixtures = async function(files) {
-  files = files.map(f => path.resolve(__dirname, `fixtures/${f}.json`));
+const loadFixtures = async function (files) {
+  files = files.map((f) => path.resolve(__dirname, `fixtures/${f}.json`));
   await fixtures.loadFiles(files, models);
 };
 
-const resetDatabase = async function() {
+const resetDatabase = async function () {
   /// clear all test data (order matters due to foreign key relationships)
   await models.sequelize.query(`
     DELETE FROM patient;
     DELETE FROM patientdelivery;
     DELETE FROM ambulance;
-    DELETE FROM hospitaladministrator;
     DELETE FROM hospitalstatusupdate;
+    DELETE FROM hospitaladministrator;
     DELETE FROM hospital;
     DELETE FROM emergencymedicalservicecall;
     DELETE FROM emergencymedicalserviceprovider;
   `);
 };
 
-beforeEach(async function() {
+beforeEach(async function () {
   await resetDatabase();
 });
 
-after(async function() {
+after(async function () {
   /// close all db connections
   await models.sequelize.close();
 });
 
 module.exports = {
-  loadFixtures
+  loadFixtures,
 };
