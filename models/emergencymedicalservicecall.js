b5dc08f94b455947b64b75388130c4caa7f88f7b
julianmclain <julianrmclain@gmail.com>
2020-08-19 20:58:10 -0700

    Update Patient and EmergencyMedicalServiceCall

--- models/emergencymedicalservicecall.js
+++ models/emergencymedicalservicecall.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class EmergencyMedicalServiceCall extends Model {
     /**
@@ -10,20 +8,24 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      EmergencyMedicalServiceCall.hasOne(models.Patient);
       // define association here
     }
-  };
-  EmergencyMedicalServiceCall.init({
-    emergencymedicalservicecall_uuid: DataTypes.UUID,
-    dispatchcallnumber: DataTypes.INTEGER,
-    startdatetime: DataTypes.DATE,
-    recordcreatetimestamp: DataTypes.DATE,
-    recordcreatesource: DataTypes.TEXT,
-    recordupdatetimestamp: DataTypes.DATE,
-    recordupdatesource: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'EmergencyMedicalServiceCall',
-  });
+  }
+  EmergencyMedicalServiceCall.init(
+    {
+      emergencymedicalservicecall_uuid: DataTypes.UUID,
+      dispatchcallnumber: DataTypes.INTEGER,
+      startdatetime: DataTypes.DATE,
+      recordcreatetimestamp: DataTypes.DATE,
+      recordcreatesource: DataTypes.TEXT,
+      recordupdatetimestamp: DataTypes.DATE,
+      recordupdatesource: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "EmergencyMedicalServiceCall",
+    }
+  );
   return EmergencyMedicalServiceCall;
 };
--- models/patient.js
+++ models/patient.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Patient extends Model {
     /**
@@ -12,115 +10,120 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
+  }
   Patient.init(
-      {
-        patient_uuid: {
-          field: "patient",
-          type: DataTypes.UUID,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        patientnumber: {
-          field: "patient",
-          type: DataTypes.INTEGER,
-          unique: true,
-          allowNull: false,
-        },
-        age: {
-          field: "age",
-          type: DataTypes.FLOAT,
-          allowNull: false,
-        },
-        sex: {
-          field: "sex",
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        stableindicator: {
-          field: "stableindicator",
-          type: DataTypes.BOOLEAN,
-          allowNull: false
-        },
-        chiefcomplaintdescription: {
-          field: "chiefcomplaintdescription",
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        heartratebpm: {
-          field: "heartratebpm",
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        temperature: {
-          field: "temperature",
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        systolicbloodpressure: {
-          field: "systolicbloodpressure",
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        diastolicbloodpressure: {
-          field: "diastolicbloodpressure",
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        respiratoryrate: {
-          field: "respiratoryrate",
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        oxygensaturation: {
-          field: "oxygensaturation",
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        ivindicator: {
-          field: "ivindicator",
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        combativebehaviorindicator: {
-          field: "combativebehaviorindicator",
-          type: DataTypes.BOOLEAN,
-          allowNull: false,
-        },
-        otherobservationnotes: {
-          field: "otherobservationnotes",
-          type: DataTypes.TEXT,
-          allowNull: false,
-        },
-        emergencymedicalservicecall_uuid: {
-          field: "emergencymedicalservicecall_uuid",
-          type: DataTypes.UUID,
-          unique: true,
-          allowNull: false,
-        },
-        recordcreatetimestamp: {
-          field: "recordcreatetimestamp",
-          type: DataTypes.DATE,
-          allowNull: false,
-        },
-        recordcreatesource: {
-          field: "recordcreatesource",
-          type: DataTypes.TEXT,
-          allowNull: false,
-        },
-        recordupdatetimestamp: {
-          field: "recordupdatetimestamp",
-          type: DataTypes.DATE,
-          allowNull: false,
-        },
-        recordupdatesource: {
-          field: "recordupdatesource",
-          type: DataTypes.STRING,
-          allowNull: false,
-        }
-  }, {
-    sequelize,
-    modelName: 'Patient',
-  });
+    {
+      id: {
+        field: "patient",
+        type: DataTypes.UUID,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      emergencyMedicalServiceCallId: {
+        field: "emergencymedicalservicecall_uuid",
+        type: DataTypes.UUID,
+        unique: true,
+        allowNull: false,
+      },
+      patientNumber: {
+        field: "patient",
+        type: DataTypes.INTEGER,
+        unique: true,
+        allowNull: false,
+      },
+      age: {
+        field: "age",
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      sex: {
+        field: "sex",
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stableIndicator: {
+        field: "stableindicator",
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      chiefComplaintDescription: {
+        field: "chiefcomplaintdescription",
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      heartRateBpm: {
+        field: "heartratebpm",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      temperature: {
+        field: "temperature",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      systolicBloodPressure: {
+        field: "systolicbloodpressure",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      diastolicBloodPressure: {
+        field: "diastolicbloodpressure",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      respiratoryRate: {
+        field: "respiratoryrate",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      oxygenSaturation: {
+        field: "oxygensaturation",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      ivIndicator: {
+        field: "ivindicator",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      combativeBehaviorIndicator: {
+        field: "combativebehaviorindicator",
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      otherObservationNotes: {
+        field: "otherobservationnotes",
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      recordCreateTimestamp: {
+        field: "recordcreatetimestamp",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      recordCreateSource: {
+        field: "recordcreatesource",
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      recordUpdateTimestamp: {
+        field: "recordupdatetimestamp",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      recordUpdateSource: {
+        field: "recordupdatesource",
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      createdAt: "recordCreateTimestamp",
+      updatedAt: "recordUpdateTimestamp",
+      tableName: "patient",
+      modelName: "Patient",
+    }
+  );
   return Patient;
 };
