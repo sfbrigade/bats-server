f4ecf34b4e60caab72e994009567e07593721312
Koome Mwiti <kmwongera@icloud.com>
2020-08-19 20:23:52 -0700

    Added more details to then models (#9)

--- models/patient.js
+++ models/patient.js
@@ -13,27 +13,111 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  Patient.init({
-    patient_uuid: DataTypes.UUID,
-    patientnumber: DataTypes.INTEGER,
-    age: DataTypes.SMALLINT,
-    sex: DataTypes.TEXT,
-    stableindicator: DataTypes.BOOLEAN,
-    chiefcomplaintdescription: DataTypes.STRING,
-    heartratebpm: DataTypes.INTEGER,
-    temperature: DataTypes.INTEGER,
-    systolicbloodpressure: DataTypes.INTEGER,
-    diastolicbloodpressure: DataTypes.INTEGER,
-    respiratoryrate: DataTypes.SMALLINT,
-    oxygensaturation: DataTypes.SMALLINT,
-    ivindicator: DataTypes.SMALLINT,
-    combativebehaviorindicator: DataTypes.BOOLEAN,
-    otherobservationnotes: DataTypes.TEXT,
-    emergencymedicalservicecall_uuid: DataTypes.UUID,
-    recordcreatetimestamp: DataTypes.DATE,
-    recordcreatesource: DataTypes.TEXT,
-    recordupdatetimestamp: DataTypes.DATE,
-    recordupdatesource: DataTypes.TEXT
+  Patient.init(
+      {
+        patient_uuid: {
+          field: "patient",
+          type: DataTypes.UUID,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        patientnumber: {
+          field: "patient",
+          type: DataTypes.INTEGER,
+          unique: true,
+          allowNull: false,
+        },
+        age: {
+          field: "age",
+          type: DataTypes.FLOAT,
+          allowNull: false,
+        },
+        sex: {
+          field: "sex",
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        stableindicator: {
+          field: "stableindicator",
+          type: DataTypes.BOOLEAN,
+          allowNull: false
+        },
+        chiefcomplaintdescription: {
+          field: "chiefcomplaintdescription",
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        heartratebpm: {
+          field: "heartratebpm",
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        temperature: {
+          field: "temperature",
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        systolicbloodpressure: {
+          field: "systolicbloodpressure",
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        diastolicbloodpressure: {
+          field: "diastolicbloodpressure",
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        respiratoryrate: {
+          field: "respiratoryrate",
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        oxygensaturation: {
+          field: "oxygensaturation",
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        ivindicator: {
+          field: "ivindicator",
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        combativebehaviorindicator: {
+          field: "combativebehaviorindicator",
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+        },
+        otherobservationnotes: {
+          field: "otherobservationnotes",
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        emergencymedicalservicecall_uuid: {
+          field: "emergencymedicalservicecall_uuid",
+          type: DataTypes.UUID,
+          unique: true,
+          allowNull: false,
+        },
+        recordcreatetimestamp: {
+          field: "recordcreatetimestamp",
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        recordcreatesource: {
+          field: "recordcreatesource",
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        recordupdatetimestamp: {
+          field: "recordupdatetimestamp",
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        recordupdatesource: {
+          field: "recordupdatesource",
+          type: DataTypes.STRING,
+          allowNull: false,
+        }
   }, {
     sequelize,
     modelName: 'Patient',
