8ec56f96e4c1b74486c7aaf95f84a1898e18af92
julianmclain <julianrmclain@gmail.com>
2020-08-25 21:04:31 -0700

    Simplify routes

--- routes/auth/index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/login", function (req, res, next) {
-  res.send("hello world");
-});
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require("express");
+const passport = require("passport");
+const authController = require("../controllers/authController");
 var router = express.Router();
 
 router.use("/api", require("./api"));
-router.use("/auth", require("./auth"));
+router.post("/login", authController.loginPost);
+router.post("/logout", authController.logout);
 
 module.exports = router;
