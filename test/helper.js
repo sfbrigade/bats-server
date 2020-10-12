bffe37e5ba31987260e8d47a4509af94c3ae8130
Francis Li <mail@francisli.com>
2020-10-11 22:25:54 -0700

    Remove dead/old code comments

--- test/helper.js
+++ test/helper.js
@@ -9,15 +9,6 @@ const models = require('../models');
 const loadFixtures = async (files) => {
   const filePaths = files.map((f) => path.resolve(__dirname, `fixtures/${f}.json`));
   await models.sequelize.transaction(async (transaction) => {
-    /// special case handling: create the first org and superuser manually, due to the circular references
-    // const org = await models.Organization.create({
-    //   id: 'f19c4a0d-227c-4929-a223-5da89f7c1d52',
-    //   name: 'Code for San Francisco',
-    //   type: 'C4SF',
-    // });
-    // const superuser = await models.User.create({
-    //   id: 'df08937c-3db7-45f6-906c-d5226c56f4f6'
-    // });
     await fixtures.loadFiles(filePaths, models, { transaction });
   });
 };
