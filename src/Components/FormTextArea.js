42c6f4f4b6fa511894f5a296cae2a27c006e6952
Francis Li <mail@francisli.com>
2020-11-27 11:55:48 -0800

    More design tweaks

--- src/Components/FormTextArea.js
+++ src/Components/FormTextArea.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 function FormInput({ label, onChange, property, required, showRequiredHint, value }) {
+  const [focused, setFocused] = useState(false);
+
   return (
     <>
-      <label htmlFor={property} className={classNames('usa-label', { 'usa-label--required': showRequiredHint && required })}>
+      <label
+        htmlFor={property}
+        className={classNames('usa-label', { 'usa-label--required': showRequiredHint && required, 'usa-label--focused': focused })}
+      >
         {label}
       </label>
       <textarea
         id={property}
         value={value || ''}
+        onBlur={() => setFocused(false)}
         onChange={(e) => onChange(property, e.target.value)}
+        onFocus={() => setFocused(true)}
         required={required}
         className="usa-textarea"
       />
