dffdcfc3e908df3e643901af570c833617e55d64
abmcbride1 <abmcbride@jasperhelps.email>
2020-11-29 19:52:42 -0800

    added styling to banner

--- src/Components/banner.js
+++ src/Components/banner.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 const Banner = (props) => {
+    const update = {
+        "color": "gray",
+        "fontSize": "20px",
+        "margin": "0em"
+    }
+    const title = {
+        "margin":"0em"
+    }
+    
     return (
         <section className="usa-banner">
-        <h1>{props.BannerTitle}</h1>
+        <h1 style={title}>{props.BannerTitle}</h1>
         {/* Functionality needed for updated date and time */}
-        <h3>Updated {props.date} @{props.time}</h3>
+        <p style={update}>Updated {props.date} @{props.time}</p>
   
         </section>
     )
