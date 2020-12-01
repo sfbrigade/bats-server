4cb4ca2aef8d2774937e6709724bd9d3619d9822
julianmclain <julianrmclain@gmail.com>
2020-11-30 21:45:35 -0800

    update css

--- src/EMS/HospitalStatusHeader.js
+++ src/EMS/HospitalStatusHeader.js
@@ -7,11 +7,11 @@ const HospitalStatusHeader = () => (
     <div className="grid-row">
       <div className="grid-col-4" />
       <div className="grid-col-8">
-        <div className="">Diversion</div>
-        <div className="">ER Beds</div>
-        <div className="">Psych Beds</div>
-        <div className="">Amb. en route</div>
-        <div className="">Amb. offloading</div>
+        <div><h3 className="header__column-name">Diversion</h3></div>
+        <div><h3 className="header__column-name">ER Beds</h3></div>
+        <div><h3 className="header__column-name">Psych Beds</h3></div>
+        <div><h3 className="header__column-name">Amb. en route</h3></div>
+        <div><h3 className="header__column-name">Amb. offloading</h3></div>
       </div>
     </div>
   </div>
--- src/EMS/HospitalStatusRow.js
+++ src/EMS/HospitalStatusRow.js
@@ -7,10 +7,10 @@ import './HospitalStatusRow.scss';
 const HospitalStatusRow = ({ hospitalStatus }) => (
   <div className="grid-container">
     <div className="grid-row">
-      <div className="grid-col-4 hospitalstatusrow__name">
-        <h3>{hospitalStatus.hospitalName}</h3>
+      <div className="grid-col-4 hospitalstatusrow__name-container">
+        <h3 className="hospitalstatusrow__name">{hospitalStatus.hospitalName}</h3>
       </div>
-      <div className="grid-col-8 hospitalstatusrow__data">
+      <div className="grid-col-8 hospitalstatusrow__data-container">
         <div className="">{hospitalStatus.diversionStatusIndicator ? "Yes" : "No"}</div>
         <div className="">{hospitalStatus.openEdBedCount}</div>
         <div className="">{hospitalStatus.openPsychBedCount}</div>
@@ -20,6 +20,8 @@ const HospitalStatusRow = ({ hospitalStatus }) => (
     </div>
   </div>
 );
+
+// TODO - how to handle date updated and hospital status notes?
 
 HospitalStatusRow.propTypes = {
   hospitalStatus: PropTypes.instanceOf(HospitalStatus).isRequired,
