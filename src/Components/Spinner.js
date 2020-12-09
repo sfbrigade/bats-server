f5c60cf3b1dbf576492896e4a7bfee9d3154e950
Francis Li <mail@francisli.com>
2020-12-09 14:22:30 -0800

    Fix class attribute

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Spinner() {
   return (
-    <div class="display-flex flex-justify-center">
-      <div class="spinner-border text-primary-vivid" role="status">
-        <span class="sr-only">Loading...</span>
+    <div className="display-flex flex-justify-center">
+      <div className="spinner-border text-primary-vivid" role="status">
+        <span className="sr-only">Loading...</span>
       </div>
     </div>
   );
