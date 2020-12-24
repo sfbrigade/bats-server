e65cfa7349606b34731b74b3d49da3525055a98b
Salasc2 <salasch2@gmail.com>
2020-12-23 16:08:41 -0800

    add expanding icon to change from plus or minus depending if content is expanded

--- src/Components/Accordian.js
+++ src/Components/Accordian.js
@@ -6,17 +6,19 @@ function Accordion(props) {
     
     const [setActive, setActiveState ] = useState("");
     const [setHeight, setHeightState] = useState("0px");
+    const [setAriaExpanded, setExpandedState] = useState("false");
 
     const content = useRef(null);
  
     function toggleAccordion() { 
         setActiveState(setActive === "" ? "active": "");
         setHeightState(setActive === "active" ? "0px": `${content.current.scrollHeight}px`);
+        setExpandedState(setActive === "active" ? "false": "true");
     }
 
     return (
         <div className="accordian-section">
-            <button className={`accordian ${setActive}`} onClick={toggleAccordion}> 
+            <button className={`usa-accordion__button ${setActive}`} onClick={toggleAccordion} aria-expanded={`${setAriaExpanded}`} aria-controls="a1"> 
                 <p className="accordian-title"> {props.title} </p>  
             </button>
             <div ref={content} style={{maxHeight: `${setHeight}`}} className="accordian-content">
