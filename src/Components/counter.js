914a86687216c2f9a2da6b214ae83b7e1fffdb22
abmcbride1 <abmcbride@jasperhelps.email>
2021-02-03 19:50:57 -0800

    worked on styling for counter

--- src/Components/counter.js
+++ src/Components/counter.js
@@ -21,9 +21,9 @@ const Counter = (props) => {
             <div className="grid-container">
   <div className="grid-row">
     <div className="grid-col-7 title">{props.CountTitle}</div>
-    <div className="grid-col-auto"><button className="usa-button circle-105 margin-05" onClick={handleDecrement}><span className="font-alt-lg position-absolute bottom-05 left-2">-</span></button></div>
-    <div className="grid-col-auto NumberBox"><div className="position-absolute bottom-05 left-2">{count}</div></div>
-    <div className="grid-col-auto"><button className="usa-button circle-2 margin-05" onClick={handleIncrement}><span className="font-alt-lg position-absolute bottom-05 left-2">+</span></button></div>
+    <div className="grid-col-auto"><button className="usa-button circle-2px padding-2" onClick={handleDecrement}><span className="font-alt-lg">-</span></button></div>
+    <div className="grid-col-auto NumberBox"><div className="margin-0 position-absolute bottom-05 left-2">{count}</div></div>
+    <div className="grid-col-auto"><button className="usa-button circle-2px padding-2" onClick={handleIncrement}><span className="font-alt-lg">+</span></button></div>
   </div>
 </div>
             
