ce2200ac66837b50fe69fcc3a701d8020068765d
Francis Li <mail@francisli.com>
2021-03-04 21:44:51 -0800

    Add counter badges to list

--- src/ER/Ringdowns.js
+++ src/ER/Ringdowns.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import RingdownCard from '../Components/RingdownCard';
 import Ringdown from '../Models/Ringdown';
 
+import './Ringdowns.scss';
+
 function Ringdowns({ ringdowns }) {
   const waiting = ringdowns.filter((r) => r.deliveryStatus === Ringdown.Status.ARRIVED || r.deliveryStatus === Ringdown.Status.OFFLOADED);
   const enroute = ringdowns.filter(
@@ -15,10 +17,12 @@ function Ringdowns({ ringdowns }) {
 
   return (
     <>
-      <div className="usa-accordion">
+      <div className="usa-accordion ringdowns">
         {waiting.length > 0 && (
           <>
-            <h3 className="usa-accordion__heading">Waiting</h3>
+            <h3 className="usa-accordion__heading">
+              Waiting <div className="ringdowns__count">{waiting.length}</div>
+            </h3>
             {waiting.map((r) => (
               <RingdownCard key={r.id} className="margin-x-3 margin-y-2" ringdown={r} />
             ))}
@@ -26,7 +30,9 @@ function Ringdowns({ ringdowns }) {
         )}
         {enroute.length > 0 && (
           <>
-            <h3 className="usa-accordion__heading">En route</h3>
+            <h3 className="usa-accordion__heading">
+              En route <div className="ringdowns__count">{enroute.length}</div>
+            </h3>
             {enroute.map((r) => (
               <RingdownCard key={r.id} className="margin-x-3 margin-y-2" ringdown={r} />
             ))}
