4241ba2b4fa3ded6d30371d204f15d21d5721779
Ethan Wright <wrightet707@gmail.com>
2021-04-07 20:30:58 -0700

    fix eslint errors

--- public/login_validations.js
+++ public/login_validations.js
@@ -1,42 +1,45 @@
-let login_btn = document.getElementById("login_submit");
-let password_error_message = document.getElementById('password_error_message');
-let password_message_success = document.getElementById('password_message_success');
-let password_label = document.getElementById('password-label');
-let password = document.getElementById("password");
+const loginBtn = document.getElementById("login_submit");
+const passwordErrorMessage = document.getElementById('password-error-message');
+const passwordMessageSuccess = document.getElementById('password_message_success');
+const passwordLabel = document.getElementById('password-label');
+const password = document.getElementById("password");
 
-password.addEventListener('input', checkPassword);
-login_btn.addEventListener("click", checkPassword);
+
 
 function checkPassword() {
 
     if (password.value === "") {
         password.classList.add('password_error');
-        password_error_message.classList.remove('password_message');
-        password_error_message.classList.remove('password_message_success');
-        password_error_message.classList.add('password_message_error');
+        passwordErrorMessage.classList.remove('password_message');
+        passwordErrorMessage.classList.remove('passwordMessageSuccess');
+        passwordErrorMessage.classList.add('password_message_error');
     }
     if (password.value !== "") {
         password.classList.add('success');
-        password_message_success.classList.remove('password_message');
-        password_error_message.classList.remove('password_message_error');
-        password_error_message.classList.add('password_message');
-        password_message_success.classList.add('password_message_success');
-        password_label.classList.add('password-label');
+        passwordMessageSuccess.classList.remove('password_message');
+        passwordErrorMessage.classList.remove('password_message_error');
+        passwordErrorMessage.classList.add('password_message');
+        passwordMessageSuccess.classList.add('password_message_success');
+        passwordLabel.classList.add('password-label');
         document.getElementById("login_submit").style.backgroundColor = null;
         document.getElementById("login_submit").style.backgroundColor = "blue";
     }
 
 }
+password.addEventListener('input', checkPassword);
+loginBtn.addEventListener("click", checkPassword);
 
-let email = document.getElementById("email");
-let email_label = document.getElementById("email-label");
-email.addEventListener("input", checkEmail);
+const email = document.getElementById("email");
+const emailLabel = document.getElementById("email-label");
+
 
 function checkEmail() {
-    let email_parts = email.value.split('@');
-    if (email.value !== '' && email_parts.length === 2) {
+    const emailParts = email.value.split('@');
+    if (email.value !== '' && emailParts.length === 2) {
         email.classList.add('email_populated');
-        email_label.classList.add('email_label_valid');
+        emailLabel.classList.add('email_label_valid');
     }
 
 }
+
+email.addEventListener("input", checkEmail);
