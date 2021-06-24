86e03d23d1d349aa2502302ff4f10e142d82aa43
Salasc2 <salasch2@gmail.com>
2021-06-23 18:18:01 -0700

    fix eslint errors

--- src/Components/PatientSummary.js
+++ src/Components/PatientSummary.js
@@ -4,16 +4,16 @@ import Ringdown from '../Models/Ringdown';
 import RingdownCard from './RingdownCard';
 import Heading from './Heading';
 
-function PatientSummary({ ringdown }) { 
-    return (
-        <>
-            <Heading title="Ringdown Overview" />
-            <RingdownCard key={ringdown.id} className="margin-x-3 margin-y-2" ringdown={ringdown} />
-        </>
-    )
+function PatientSummary({ ringdown }) {
+  return (
+    <>
+      <Heading title="Ringdown Overview" />
+      <RingdownCard key={ringdown.id} className="margin-x-3 margin-y-2" ringdown={ringdown} />
+    </>
+  );
 }
 
 PatientSummary.propTypes = {
-    ringdown: PropTypes.instanceOf(Ringdown).isRequired
+  ringdown: PropTypes.instanceOf(Ringdown).isRequired,
 };
 export default PatientSummary;
