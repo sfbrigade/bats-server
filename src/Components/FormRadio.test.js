4940af920fbca2d0637305402e5e6a4c3e39c494
Derek Velzy <dmvelzy@gmail.com>
2021-08-02 19:56:34 -0500

    add radio input onchange test

--- src/Components/FormRadio.test.js
+++ src/Components/FormRadio.test.js
@@ -39,3 +39,47 @@ test('Snapshot', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
+
+test('FormInput should trigger the onChange callback', () => {
+  const mock1 = jest.fn();
+  const mock2 = jest.fn();
+  const mock3 = jest.fn();
+
+  const value = 'APPLE';
+
+  const wrapper = render(
+    <FormRadioFieldSet
+      labelText="Select Favorite Fruit"
+      property="fruit"
+      isRequired
+      validationState='FIXED'
+    >
+      <FormRadio
+        onChange={mock1}
+        currentValue={value}
+        label="Apple"
+        property="fruit"
+        value="APPLE"
+      />
+      <FormRadio
+        onChange={mock2}
+        currentValue={value}
+        label="Banana"
+        property="fruit"
+        value="BANANA"
+      />
+      <FormRadio
+        onChange={mock3}
+        currentValue={value}
+        label="Coconut"
+        property="fruit"
+        value="COCONUT"
+      />
+    </FormRadioFieldSet>
+  );
+
+    const radio = wrapper.getByLabelText('Banana');
+    fireEvent.click(radio);
+    expect(mock2.mock.calls.length).toBe(1);
+    expect(mock1.mock.calls.length).toBe(0);
+})
