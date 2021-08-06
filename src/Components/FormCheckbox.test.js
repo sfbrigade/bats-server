375a3e9296996771a26693c97af52ae6474f21b4
Derek Velzy <dmvelzy@gmail.com>
2021-08-05 21:30:07 -0500

    add testing for checkbox

---
+++ src/Components/FormCheckbox.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { fireEvent, render } from '@testing-library/react';
+import FormCheckbox from './FormCheckbox';
+
+test('Snapshot', () => {
+  const component = renderer.create(
+    <FormCheckbox
+      currentValue="apple"
+      label="banana"
+      onChange={() => {}}
+      property="banana"
+      value={true}
+    />);
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('FormInput should trigger the onChange callback', () => {
+  const mock = jest.fn();
+
+  const wrapper = render(
+    <FormCheckbox
+      currentValue="apple"
+      label="Coconut"
+      onChange={mock}
+      property="coconut"
+      value={true}
+    />);
+
+    const checkbox = wrapper.getByLabelText('Coconut');
+    fireEvent.click(checkbox);
+    expect(mock.mock.calls.length).toBe(1);
+})
