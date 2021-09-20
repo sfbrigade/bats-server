3fc62ee8d92cdf3c4ea3e4b86c6066f05e509d48
abmcbride5 <abmcbride5@gmail.com>
2021-09-20 00:06:18 -0700

    updated ER Dashboard

--- src/Admin/AdminER/ErDashboard.js
+++ src/Admin/AdminER/ErDashboard.js
@@ -5,33 +5,33 @@ import Heading from '../../Components/Heading';
 export default function ErDashboard({ more }) {
 
     return (
-        <div className="margin-left-5">
+        <div className="margin-left-5 ">
             <div >
             <Heading title="Active Nurses" />
             <table >
                 <tr>
-                    <th className="padding-2 margin-3">Status</th>
-                    <th className="padding-2 margin-3">Name</th>
-                    <th className="padding-2 margin-3">Shift</th>
+                    <th className="padding-2">Status</th>
+                    <th className="padding-2">Name</th>
+                    <th className="padding-2">Shift</th>
                     
                 </tr>
-                <tr>
-                    <td className="padding-2 margin-3">Active</td>
-                    <td className="padding-2 margin-3">Molly Jane</td>
-                    <td className="padding-2 margin-3">Day shift</td>
-                    <td className="padding-2 margin-3"><button onClick={() => more()}>More</button></td>
+                <tr className="bg-base text-base-lightest">
+                    <td className="padding-2">Active</td>
+                    <td className="padding-2">Molly Jane</td>
+                    <td className="padding-2">Day shift</td>
+                    <td className="padding-2"><button className="bg-base text-base-lightest border-0" onClick={() => more()}>More</button></td>
                 </tr>
-                <tr>
-                    <td className="padding-2 margin-3">Inactive</td>
-                    <td className="padding-2 margin-3">John Doe</td>
-                    <td className="padding-2 margin-3">Day shift</td>
-                    <td className="padding-2 margin-3"><button onClick={() => more()}>More</button></td>
+                <tr className="bg-base text-base-lightest">
+                    <td className="padding-2">Inactive</td>
+                    <td className="padding-2">John Doe</td>
+                    <td className="padding-2">Day shift</td>
+                    <td className="padding-2"><button className="bg-base text-base-lightest border-0" onClick={() => more()}>More</button></td>
                 </tr>
-                <tr>
-                    <td className="padding-2 margin-3">Inactive</td>
-                    <td className="padding-2 margin-3">Cindy Moton</td>
-                    <td className="padding-2 margin-3">mid shift</td>
-                    <td className="padding-2 margin-3"><button onClick={() => more()}>More</button></td>     
+                <tr className="bg-base text-base-lightest">
+                    <td className="padding-2">Inactive</td>
+                    <td className="padding-2">Cindy Moton</td>
+                    <td className="padding-2">mid shift</td>
+                    <td className="padding-2"><button className="bg-base text-base-lightest border-0"  onClick={() => more()}>More</button></td>     
                 </tr>
                 </table>
             </div>
