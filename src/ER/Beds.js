1a73ec629b903c512f6d41e55e4632f5c241e1f6
Francis Li <mail@francisli.com>
2021-11-03 18:19:16 -0700

    Fix build failure

--- src/ER/Beds.js
+++ src/ER/Beds.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { DateTime } from 'luxon';
-// import classNames from 'classnames';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import ApiService from '../ApiService';
 
-// import Alert from '../Components/Alert';
+import Alert from '../Components/Alert';
 import Counter from '../Components/Counter';
 import FormTextArea from '../Components/FormTextArea';
 import Heading from '../Components/Heading';
@@ -16,8 +16,8 @@ import './Beds.scss';
 function Beds({ statusUpdate, onStatusUpdate }) {
   const [additionalNotes, setAdditionalNotes] = useState(null);
   const [showNotesUpdated, setShowNotesUpdated] = useState(false);
-  // const [showUpdate, setShowUpdate] = useState(false);
-  // const [showConfirmUpdate, setShowConfirmUpdate] = useState(false);
+  const [showUpdate, setShowUpdate] = useState(false);
+  const [showConfirmUpdate, setShowConfirmUpdate] = useState(false);
   const [editBeds, setEditBeds] = useState(false);
   const [editNotes, setEditNotes] = useState(false);
 
@@ -49,16 +49,16 @@ function Beds({ statusUpdate, onStatusUpdate }) {
     setEditNotes(false);
   }
 
-  // function handleDiversionUpdate() {
-  //   const newStatusUpdate = new HospitalStatus(statusUpdate);
-  //   newStatusUpdate.updateDateTimeLocal = DateTime.local().toISO();
-  //   newStatusUpdate.divertStatusUpdateDateTimeLocal = newStatusUpdate.updateDateTimeLocal;
-  //   newStatusUpdate.divertStatusIndicator = !newStatusUpdate.divertStatusIndicator;
-  //   onStatusUpdate(newStatusUpdate);
-  //   ApiService.hospitalStatuses.create(newStatusUpdate.toJSON());
-  //   setShowUpdate(false);
-  //   setShowConfirmUpdate(true);
-  // }
+  function handleDiversionUpdate() {
+    const newStatusUpdate = new HospitalStatus(statusUpdate);
+    newStatusUpdate.updateDateTimeLocal = DateTime.local().toISO();
+    newStatusUpdate.divertStatusUpdateDateTimeLocal = newStatusUpdate.updateDateTimeLocal;
+    newStatusUpdate.divertStatusIndicator = !newStatusUpdate.divertStatusIndicator;
+    onStatusUpdate(newStatusUpdate);
+    ApiService.hospitalStatuses.create(newStatusUpdate.toJSON());
+    setShowUpdate(false);
+    setShowConfirmUpdate(true);
+  }
 
   return (
     <div className="usa-accordion">
@@ -133,7 +133,7 @@ function Beds({ statusUpdate, onStatusUpdate }) {
       {/* <Heading
         title="Diversion status"
         subtitle={`Updated ${DateTime.fromISO(statusUpdate.divertStatusUpdateDateTimeLocal).toFormat('M/d/yyyy @ H:mm')}`}
-      /> */}
+      />
       <div className="usa-accordion__content hide-diversion-status">
         <div className="usa-fieldset">
           <div className={classNames('beds__diversion', { 'beds__diversion--on': statusUpdate.divertStatusIndicator })}>
