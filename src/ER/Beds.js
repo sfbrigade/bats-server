440233d4fe1709cec2a4a3492096ccd3ec72df6b
abmcbride5 <43585996+abmcbride5@users.noreply.github.com>
2021-12-01 18:20:04 -0800

    [#102] Bed count update gate (#105)
    * create bed count update only page for SF General AOD
    
    * commented out diversion status
    
    * rough draft of bed count with read only/ editable view
    
    * corrected er bed count
    
    * created migration to add isAod column to hospitaluser table, edited redirect for aod
    
    * removed disable attribute from formtextarea
    
    * Fix build failure
    
    * Add db dependency to server container
    
    * updated migration for hospitaluser, removed AOD.js file
    
    * updated ER.js to accommodate info tab user only view
    
    * update gate functionality created, needs styling, work in porgress
    
    * Remove unused state
    
    * Revert older changes from branch
    
    * Styling updates
    
    Co-authored-by: Francis Li <mail@francisli.com>
    Co-authored-by: Francis Li <francisli@users.noreply.github.com>

--- src/ER/Beds.js
+++ src/ER/Beds.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import { DateTime } from 'luxon';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import ApiService from '../ApiService';
 
-import Alert from '../Components/Alert';
 import Counter from '../Components/Counter';
 import FormTextArea from '../Components/FormTextArea';
 import Heading from '../Components/Heading';
@@ -13,138 +11,76 @@ import HospitalStatus from '../Models/HospitalStatus';
 
 import './Beds.scss';
 
-function Beds({ statusUpdate, onStatusUpdate }) {
+function Beds({ statusUpdate, onStatusUpdate, incomingRingdownsCount }) {
   const [additionalNotes, setAdditionalNotes] = useState(null);
-  const [showNotesUpdated, setShowNotesUpdated] = useState(false);
-  const [showUpdate, setShowUpdate] = useState(false);
-  const [showConfirmUpdate, setShowConfirmUpdate] = useState(false);
+  const [isEditing, setEditing] = useState(false);
 
-  function handleBedUpdate(event) {
+  function handleChange(event) {
     const newStatusUpdate = new HospitalStatus(statusUpdate);
-    newStatusUpdate.updateDateTimeLocal = DateTime.local().toISO();
-    newStatusUpdate.bedCountUpdateDateTimeLocal = newStatusUpdate.updateDateTimeLocal;
     if (event.target.name === 'erBedsCount') {
       newStatusUpdate.openEdBedCount = event.target.value;
     } else if (event.target.name === 'psychBedsCount') {
       newStatusUpdate.openPsychBedCount = event.target.value;
     }
     onStatusUpdate(newStatusUpdate);
-    ApiService.hospitalStatuses.create(newStatusUpdate.toJSON());
   }
 
-  function handleNotesUpdate() {
-    const newStatusUpdate = new HospitalStatus(statusUpdate);
-    newStatusUpdate.updateDateTimeLocal = DateTime.local().toISO();
-    newStatusUpdate.notesUpdateDateTimeLocal = newStatusUpdate.updateDateTimeLocal;
-    newStatusUpdate.additionalServiceAvailabilityNotes = additionalNotes;
-    onStatusUpdate(newStatusUpdate);
-    ApiService.hospitalStatuses.create(newStatusUpdate.toJSON());
-    setAdditionalNotes(null);
-    setShowNotesUpdated(true);
-    setTimeout(() => {
-      setShowNotesUpdated(false);
-    }, 1000);
-  }
+  function handleUpdate() {
+    statusUpdate.updateDateTimeLocal = DateTime.local().toISO();
+    if (additionalNotes !== null) {
+      statusUpdate.additionalServiceAvailabilityNotes = additionalNotes;
+      setAdditionalNotes(null);
+    }
 
-  function handleDiversionUpdate() {
-    const newStatusUpdate = new HospitalStatus(statusUpdate);
-    newStatusUpdate.updateDateTimeLocal = DateTime.local().toISO();
-    newStatusUpdate.divertStatusUpdateDateTimeLocal = newStatusUpdate.updateDateTimeLocal;
-    newStatusUpdate.divertStatusIndicator = !newStatusUpdate.divertStatusIndicator;
-    onStatusUpdate(newStatusUpdate);
-    ApiService.hospitalStatuses.create(newStatusUpdate.toJSON());
-    setShowUpdate(false);
-    setShowConfirmUpdate(true);
+    ApiService.hospitalStatuses.create(statusUpdate.toJSON());
+    setEditing(false);
   }
 
   return (
-    <div className="usa-accordion">
-      <Heading
-        title="Bed availability"
-        subtitle={`Updated ${DateTime.fromISO(statusUpdate.bedCountUpdateDateTimeLocal).toFormat('M/d/yyyy @ H:mm')}`}
-      />
+    <div className="usa-accordion beds">
       <div className="usa-accordion__content">
         <form className="usa-form">
+          <Heading title="Available Beds">
+            {incomingRingdownsCount > 0 && <span className="beds__incoming">{incomingRingdownsCount} incoming</span>}
+          </Heading>
           <fieldset className="usa-fieldset beds__availability">
-            <Counter label="ER Beds" name="erBedsCount" min={0} onChange={handleBedUpdate} value={statusUpdate.openEdBedCount} />
             <Counter
+              isEditing={isEditing}
+              label="ER Beds"
+              name="erBedsCount"
+              min={0}
+              onChange={handleChange}
+              value={statusUpdate.openEdBedCount}
+            />
+            <Counter
+              isEditing={isEditing}
               label="Behavioral Beds"
               name="psychBedsCount"
               min={0}
-              onChange={handleBedUpdate}
+              onChange={handleChange}
               value={statusUpdate.openPsychBedCount}
             />
           </fieldset>
-        </form>
-      </div>
-      <Heading
-        title="Additional Notes"
-        subtitle={`Updated ${DateTime.fromISO(statusUpdate.notesUpdateDateTimeLocal).toFormat('M/d/yyyy @ H:mm')}`}
-      />
-      <div className="usa-accordion__content">
-        <form className="usa-form">
+          <Heading title="ER conditions" />
           <fieldset className="usa-fieldset beds__notes">
             <FormTextArea
-              label={
-                <>
-                  ER conditions <span>(optional)</span>
-                </>
-              }
               property="additionalNotes"
+              disabled={!isEditing}
               value={additionalNotes == null ? statusUpdate.additionalServiceAvailabilityNotes : additionalNotes}
               onChange={(property, value) => setAdditionalNotes(value)}
             />
-            <div className="beds__notes-controls">
-              <span className="beds__updated">
-                {showNotesUpdated && (
-                  <>
-                    <i className="fas fa-check-circle" />
-                    &nbsp;Updated
-                  </>
-                )}
-              </span>
-              <button disabled={additionalNotes == null} className="usa-button" type="button" onClick={handleNotesUpdate}>
-                Update
+            {!isEditing ? (
+              <button className="usa-button usa-button--outline" type="button" onClick={() => setEditing(true)}>
+                Update Hospital Info
               </button>
-            </div>
+            ) : (
+              <button className="usa-button" type="button" onClick={handleUpdate}>
+                Confirm Updates
+              </button>
+            )}
+            <div className="beds__updated">Updated {DateTime.fromISO(statusUpdate.updateDateTimeLocal).toFormat('M/d/yyyy @ H:mm')}</div>
           </fieldset>
         </form>
-      </div>
-
-      {/* <Heading
-        title="Diversion status"
-        subtitle={`Updated ${DateTime.fromISO(statusUpdate.divertStatusUpdateDateTimeLocal).toFormat('M/d/yyyy @ H:mm')}`}
-      /> */}
-      <div className="usa-accordion__content hide-diversion-status">
-        <div className="usa-fieldset">
-          <div className={classNames('beds__diversion', { 'beds__diversion--on': statusUpdate.divertStatusIndicator })}>
-            {statusUpdate.divertStatusIndicator && <>On diversion</>}
-            {!statusUpdate.divertStatusIndicator && <>Not on diversion</>}
-            <button type="button" className="usa-button--unstyled beds__change-status" onClick={() => setShowUpdate(true)}>
-              Change status
-            </button>
-            {showUpdate && (
-              <Alert
-                type="warning"
-                title="Update divert status?"
-                message="Ambulances will be notified."
-                cancel="Keep status"
-                destructive="Update status"
-                onDestructive={handleDiversionUpdate}
-                onCancel={() => setShowUpdate(false)}
-              />
-            )}
-            {showConfirmUpdate && (
-              <Alert
-                type="success"
-                title="Divert status updated"
-                message="Ambulances will be notified."
-                primary="Return to form"
-                onPrimary={() => setShowConfirmUpdate(false)}
-              />
-            )}
-          </div>
-        </div>
       </div>
     </div>
   );
@@ -153,6 +89,7 @@ function Beds({ statusUpdate, onStatusUpdate }) {
 Beds.propTypes = {
   onStatusUpdate: PropTypes.func.isRequired,
   statusUpdate: PropTypes.instanceOf(HospitalStatus).isRequired,
+  incomingRingdownsCount: PropTypes.number.isRequired,
 };
 
 export default Beds;
