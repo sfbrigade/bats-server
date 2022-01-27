cce0e0b49f12c2002321b1e04c5bea4a9834d3b6
Francis Li <mail@francisli.com>
2022-01-26 19:14:46 -0800

    Add OFFLOADED CONFIRMED state in db enum

---
+++ migrations/20220127031019-add-offloaded-confirmed-state.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.sequelize.query(`ALTER TYPE deliverystatus ADD VALUE 'OFFLOADED ACKNOWLEGED' BEFORE 'RETURNED TO SERVICE'`);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.sequelize.query(`DELETE FROM patientdeliveryupdate WHERE deliverystatusenum='OFFLOADED ACKNOWLEGED';`);
+    await queryInterface.sequelize.query(`ALTER TYPE deliverystatus RENAME TO deliverystatus_old;`);
+    await queryInterface.sequelize.query(
+      `CREATE TYPE deliverystatus AS ENUM ('RINGDOWN SENT', 'RINGDOWN RECEIVED', 'RINGDOWN CONFIRMED', 'ARRIVED', 'OFFLOADED', 'RETURNED TO SERVICE', 'CANCELLED', 'CANCEL ACKNOWLEGED', 'REDIRECTED', 'REDIRECT ACKNOWLEGED');`
+    );
+    await queryInterface.sequelize.query(
+      `ALTER TABLE patientdeliveryupdate ALTER COLUMN deliverystatusenum TYPE deliverystatus USING deliverystatusenum::text::deliverystatus;`
+    );
+    await queryInterface.sequelize.query(
+      `ALTER TABLE patientdelivery ALTER COLUMN currentdeliverystatusenum TYPE deliverystatus USING currentdeliverystatusenum::text::deliverystatus;`
+    );
+    await queryInterface.sequelize.query(`DROP TYPE deliverystatus_old;`);
+  },
+};
