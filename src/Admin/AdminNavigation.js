d8cbffea3e182d69072a25303f8d9516e81bb111
Francis Li <mail@francisli.com>
2022-02-24 11:00:31 -0800

    First pass, navigation styling tweaks

--- src/Admin/AdminNavigation.js
+++ src/Admin/AdminNavigation.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { NavLink, useRouteMatch } from 'react-router-dom';
+import { Link, NavLink, useRouteMatch } from 'react-router-dom';
 
 import Context from '../Context';
 
@@ -9,33 +9,32 @@ function AdminNavigation() {
   const { url } = useRouteMatch();
   const { user } = useContext(Context);
 
-  const linkStyle = {
-    color: '#a9aeb1',
-    margin: '0% 6% 0% 0%',
-    position: 'relative',
-    top: '4rem',
-    fontSize: '24px',
-  };
-
   return (
-    <div className="margin-y-6 padding-bottom-9">
-      <a className="text-base-darkest adminnav_login_logout" href="/auth/local/logout">
-        Logout
-      </a>
-      <div className="adminnav_logo" />
-      <h2 className="adminnav_hospital_name">{user ? user.activeHospitals[0].hospital.name : ''}</h2>
-      <button type="button" className=" adminnav_edit_profile border-0 border-bottom">
-        Edit Profile
-      </button>
-      <NavLink to={`${url}/dashboard`} style={linkStyle}>
-        Dashboard
-      </NavLink>
-      <NavLink to={`${url}/users`} style={linkStyle}>
-        Users
-      </NavLink>
-      <NavLink to={`${url}/ringdowns`} style={linkStyle}>
-        Ringdowns
-      </NavLink>
+    <div className="admin-navigation">
+      <div className="grid-container">
+        <div className="display-flex flex-row flex-justify">
+          <div>
+            <h2 className="admin-navigation__hospital-name">{user?.activeHospitals?.[0]?.hospital?.name}</h2>
+            <Link to={`/users/${user?.id}`}>Edit Profile</Link>
+          </div>
+          <div>
+            <a className="admin-navigation__logout" href="/auth/local/logout">
+              Logout
+            </a>
+          </div>
+        </div>
+        <div className="display-flex flex-row flex-justify-center">
+          <NavLink to={`${url}/dashboard`} className="admin-navigation__link" activeClassName="admin-navigation__link--active">
+            Dashboard
+          </NavLink>
+          <NavLink to={`${url}/users`} className="admin-navigation__link" activeClassName="admin-navigation__link--active">
+            Users
+          </NavLink>
+          <NavLink to={`${url}/ringdowns`} className="admin-navigation__link" activeClassName="admin-navigation__link--active">
+            Ringdowns
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
