f1a1c4e1d8e449f484f30ee15cea0a0f6366e467
Francis Li <francisli@users.noreply.github.com>
2022-03-18 10:49:24 -0700

    [#134] Return HTTP 401 Unauthorized from websocket when not logged in (#135)
    * [#134] Return HTTP 401 Unauthorized from websocket when not logged in
    
    * Add more HTTP error codes before disconnection

--- wss.js
+++ wss.js
@@ -113,6 +113,7 @@ function configure(server, app) {
         req.user = await models.User.findByPk(req.session.passport.user);
       }
       if (!req.user) {
+        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
         socket.destroy();
         return;
       }
@@ -130,6 +131,7 @@ function configure(server, app) {
             req.hospital = await models.Hospital.findByPk(query.id);
           }
           if (!req.hospital) {
+            socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
             socket.destroy();
             return;
           }
@@ -138,6 +140,7 @@ function configure(server, app) {
           });
           break;
         default:
+          socket.write('HTTP/1.1 403 Forbidden\r\n\r\n');
           socket.destroy();
       }
     });
