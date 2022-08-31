808062eadd9119c568e35cd22a20d4bfdd5a730e
John Dunning <fw@johndunning.com>
2022-08-31 16:13:02 -0700

    [Fix #211] Make new ringdown modal the full height of the window (#220)
    * Make new ringdown modal the full height of the window
    
    Fix #211
    Set the header to 100vh and position: fixed.
    Add a no-scrolling class to html so the body won't scroll while the modal is open.
    
    * Let overlay scroll when needed
    
    Set a max-height so it won't touch the header.
    
    * Fix modal overlay layout in all cases
    
    Add .minh-100vh class to the ER container so it stretches the full viewport height even on the hospital tab.  Without that, the overlay contents get clipped on the bottom.
    Switch Header back to position: sticky.
    Switch UnconfirmedRingdowns back to position: absolute and make it 100vh high, so it doesn't stick to the bottom of the content.
    
    Co-authored-by: Francis Li <mail@francisli.com>

--- src/ER/ER.js
+++ src/ER/ER.js
@@ -73,8 +73,16 @@ export default function ER() {
     }
   }, [lastMessage, setRingdowns, setUnconfirmedRingdowns, setStatusUpdate, showRingdown, playSound]);
 
+  useEffect(() => {
+    if (hasUnconfirmedRingdowns) {
+      document.documentElement.classList.add('no-scrolling');
+    } else {
+      document.documentElement.classList.remove('no-scrolling');
+    }
+  }, [hasUnconfirmedRingdowns]);
+
   return (
-    <div className="grid-container">
+    <div className="grid-container minh-100vh">
       <div className="grid-row">
         <div className="tablet:grid-col-6 tablet:grid-offset-3">
           <RoutedHeader selectedTab={selectedTab} onSelect={setSelectedTab} />
