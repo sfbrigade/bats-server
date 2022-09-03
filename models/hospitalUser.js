347748b1e1bba186c121e499adf6028206fdc838
John Dunning <fw@johndunning.com>
2022-09-03 09:20:52 -0700

    Add metadata for hospitalUser

--- models/hospitalUser.js
+++ models/hospitalUser.js
@@ -1,7 +1,9 @@
 const _ = require('lodash');
 const { Model } = require('sequelize');
+const metadata = require('../src/metadata/hospitalUser');
+const convertToSequelizeField = require('../src/metadata/convertToSequelizeField');
 
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize) => {
   class HospitalUser extends Model {
     static associate(models) {
       HospitalUser.belongsTo(models.Hospital);
@@ -18,68 +20,12 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
 
-  HospitalUser.init(
-    {
-      id: {
-        field: 'hospitaluser_uuid',
-        type: DataTypes.UUID,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      HospitalId: {
-        field: 'hospital_uuid',
-        type: DataTypes.UUID,
-        unique: true,
-        allowNull: false,
-      },
-      EdAdminUserId: {
-        field: 'edadminuser_uuid',
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      isActive: {
-        field: 'activeindicator',
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true,
-      },
-      isInfoUser: {
-        field: 'infouserindicator',
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-        allowNull: false,
-      },
-      isRingdownUser: {
-        field: 'ringdownuserindicator',
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-        allowNull: false,
-      },
-      createdAt: {
-        field: 'recordcreatetimestamp',
-        type: DataTypes.DATE,
-      },
-      CreatedById: {
-        field: 'recordcreateuser_uuid',
-        type: DataTypes.UUID,
-      },
-      updatedAt: {
-        field: 'recordupdatetimestamp',
-        type: DataTypes.DATE,
-      },
-      UpdatedById: {
-        field: 'recordupdateuser_uuid',
-        type: DataTypes.UUID,
-      },
-    },
-    {
-      sequelize,
-      timestamps: true,
-      tableName: 'hospitaluser',
-      modelName: 'HospitalUser',
-    }
-  );
+  HospitalUser.init(metadata.getFieldHash(convertToSequelizeField), {
+    sequelize,
+    timestamps: true,
+    tableName: metadata.tableName,
+    modelName: metadata.modelName,
+  });
 
   HospitalUser.addScope('active', {
     where: { isActive: true },
---
+++ src/metadata/hospitalUser.js
@@ -0,0 +1,48 @@
+const ModelMetadata = require('./ModelMetadata');
+
+const fields = [
+  {
+    name: 'id',
+    colName: 'hospitaluser_uuid',
+    type: 'uuid',
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  {
+    name: 'HospitalId',
+    colName: 'hospital_uuid',
+    type: 'uuid',
+    unique: true,
+    allowNull: false,
+  },
+  {
+    name: 'EdAdminUserId',
+    colName: 'edadminuser_uuid',
+    type: 'uuid',
+    unique: true,
+    allowNull: false,
+  },
+  {
+    name: 'isActive',
+    colName: 'activeindicator',
+    type: 'boolean',
+    allowNull: false,
+    defaultValue: true,
+  },
+  {
+    name: 'isInfoUser',
+    colName: 'infouserindicator',
+    type: 'boolean',
+    allowNull: false,
+    defaultValue: true,
+  },
+  {
+    name: 'isRingdownUser',
+    colName: 'ringdownuserindicator',
+    type: 'boolean',
+    allowNull: false,
+    defaultValue: true,
+  },
+];
+
+module.exports = new ModelMetadata({ modelName: 'HospitalUser', fields });
