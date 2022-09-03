56b95c43501553d7cf1758b55f5b670fde4a1fca
John Dunning <fw@johndunning.com>
2022-09-03 13:55:32 -0700

    Add metadata for user
    Add support in convertToSequelizeField() for sequelize getters/setters, validate key and virtual type fields.

--- src/metadata/convertToSequelizeField.js
+++ src/metadata/convertToSequelizeField.js
@@ -1,22 +1,37 @@
 const { DataTypes } = require('sequelize');
 
-const SequelizeKeys = ['allowNull', 'autoIncrement', 'defaultValue', 'primaryKey', 'unique'];
+const SequelizeKeys = ['allowNull', 'autoIncrement', 'defaultValue', 'get', 'primaryKey', 'set', 'unique', 'validate'];
 
 const pick = (obj, keys) => Object.fromEntries(keys.filter((key) => key in obj).map((key) => [key, obj[key]]));
 
-module.exports = function convertToSequelizeField(field) {
-  const { name, type: typeName, enumValues } = field;
+const getDataType = (typeName) => DataTypes[typeName.toUpperCase()];
 
-  let type = DataTypes[typeName.toUpperCase()];
+module.exports = function convertToSequelizeField(field) {
+  const { name, colName, enumValues, virtualArgs, type: typeName } = field;
+  const sqlAttributes = pick(field, SequelizeKeys);
+  let type = getDataType(typeName);
 
   if (typeName === 'enum') {
     type = DataTypes.ENUM(enumValues);
+  } else if (typeName === 'virtual' && virtualArgs) {
+    // eslint-disable-next-line prefer-const
+    let [returnType, fields] = virtualArgs;
+
+    if (returnType) {
+      returnType = getDataType(returnType);
+    }
+
+    type = new DataTypes.VIRTUAL(returnType, fields);
+  }
+
+  // virtual fields don't have a column name in the db
+  if (typeName !== 'virtual') {
+    sqlAttributes.field = colName;
   }
 
   const convertedField = {
-    field: field.colName,
     type,
-    ...pick(field, SequelizeKeys),
+    ...sqlAttributes,
   };
 
   return [name, convertedField];
