37d7ae562aec5d0273d3c06fd420f801ccaf2dd8
John Dunning <fw@johndunning.com>
2022-09-06 16:21:35 -0700

    Create payload sub-objects and defaults programmatically
    Add metadata/index.js.
    Change createValidationData() to setValidationData(),.
    Get rid of ambulance and emsCall getters defined in class.

--- src/Models/Ringdown.js
+++ src/Models/Ringdown.js
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon';
 import PropTypes from 'prop-types';
 import { PatientFieldData, ValidationState } from './PatientFieldData';
-import patient from '../shared/metadata/patient';
+import * as metadata from '../shared/metadata';
 import convertToPropType from '../shared/convertToPropType';
 import DeliveryStatus from '../shared/constants/DeliveryStatus';
 
@@ -38,7 +38,8 @@ function overrideSetter(target, key, setter) {
 }
 
 // specify the fields that must all have valid input to make the ringdown valid.  the second array item is an optional function to determine
-// whether the field's current value is valid as input.  the array order should be the same as the field order in PatientFields.
+// whether the field's current value is valid as input.  by default, the field is counted as having input if its value is truthy.   the
+// array order should be the same as the field order in PatientFields.
 const validatedFields = [
   ['ambulanceIdentifier'],
   ['dispatchCallNumber'],
@@ -50,25 +51,68 @@ const validatedFields = [
   ['all', () => ValidationState.NO_INPUT],
 ];
 
+const payloadModels = [
+  ['ambulance', ['ambulanceIdentifier']],
+  [['emergencyMedicalServiceCall', 'emsCall'], ['dispatchCallNumber']],
+  ['hospital', []],
+//  ['hospital', [['id', 'hospitalId']]],
+  ['patient', metadata.patient.getObjectFields()],
+  ['patientDelivery', ['etaMinutes', 'currentDeliveryStatus']],
+];
+
+// build a hash with an empty default object for each sub-object in the payload
+const createDefaultPayload = () => ({
+  ...payloadModels.reduce((result, [name]) => ({ ...result, [name instanceof Array ? name[1] : name]: {} }), {}),
+  // default the urgency to Code 2 for a fresh ringdown if the Code 3 option has been disabled
+  patient: (window.env.REACT_APP_DISABLE_CODE_3 && { emergencyServiceResponseType: 'CODE 2' }) || {},
+});
+
 class Ringdown {
   static get Status() {
     return DeliveryStatus;
   }
 
+  static ascendingByOrder(a, b) {
+    if (a.order < b.order) {
+      return -1;
+    }
+    if (a.order === b.order) {
+      return 0;
+    }
+    return 1;
+  }
+
   constructor(payload, validationData) {
-    this.payload = payload || {};
-    this.payload.ambulance = this.payload.ambulance || {};
-    this.payload.emsCall = this.payload.emsCall || {};
-    this.payload.hospital = this.payload.hospital || {};
-    // default the urgency to Code 2 for a fresh ringdown if the Code 3 option has been disabled
-    this.payload.patient =
-      this.payload.patient || (window.env.REACT_APP_DISABLE_CODE_3 && { emergencyServiceResponseType: 'CODE 2' }) || {};
-    this.payload.patientDelivery = this.payload.patientDelivery || {};
+    this.payload = {
+      ...createDefaultPayload(),
+      ...payload,
+    };
 
-    // add getters/setters for patient fields
-    attachFields(this, patient.getObjectFields(), this.payload.patient);
+    payloadModels.forEach(([modelInfo, fieldNames]) => {
+      let metadataName = modelInfo;
+      let objectName = modelInfo;
 
-    // add custom setters for these payload fields, since their settings affect other fields
+      if (modelInfo instanceof Array) {
+        [metadataName, objectName] = modelInfo;
+      }
+      const fields = typeof fieldNames[0] === 'string'
+        // get the fields listed by name in payloadModels
+        ? metadata[metadataName].getFields(undefined, ({ name }) => fieldNames.includes(name))
+        // this model's fields have already been extracted
+        : fieldNames;
+      const self = this;
+
+      // add a getter/setter for each field
+      attachFields(this, fields, this.payload[objectName]);
+      // add a getter to return this payload sub-object
+      this[objectName] = {
+        get() { return self.payload[objectName]; },
+        configurable: true,
+        enumerable: true,
+      };
+    });
+
+    // add custom setters for these payload fields, since their values affect other fields
     overrideSetter(this, 'lowOxygenResponseType', (newValue) => {
       this.payload.patient.lowOxygenResponseType = newValue;
       if (newValue !== 'SUPPLEMENTAL OXYGEN') {
@@ -80,7 +124,7 @@ class Ringdown {
       this.restraintIndicator = newValue && this.restraintIndicator;
     });
 
-    this.validationData = validationData || this.createValidationData(validatedFields);
+    this.setValidationData(validationData)
   }
 
   clone() {
@@ -96,39 +140,7 @@ class Ringdown {
     return this.payload.id;
   }
 
-  // Ambulance
-
-  get ambulance() {
-    return this.payload.ambulance ?? {};
-  }
-
-  get ambulanceIdentifier() {
-    return this.payload.ambulance.ambulanceIdentifier ?? '';
-  }
-
-  set ambulanceIdentifier(newValue) {
-    this.payload.ambulance.ambulanceIdentifier = newValue;
-  }
-
-  // EMS Call
-
-  get emsCall() {
-    return this.payload.emsCall ?? {};
-  }
-
-  get dispatchCallNumber() {
-    return this.payload.emsCall.dispatchCallNumber ?? '';
-  }
-
-  set dispatchCallNumber(newValue) {
-    this.payload.emsCall.dispatchCallNumber = newValue;
-  }
-
   // Hospital
-
-  get hospital() {
-    return this.payload.hospital ?? {};
-  }
 
   get hospitalId() {
     return this.payload.hospital.id ?? null;
@@ -178,22 +190,6 @@ class Ringdown {
       : null;
   }
 
-  get etaMinutes() {
-    return this.payload.patientDelivery.etaMinutes ?? null;
-  }
-
-  set etaMinutes(newValue) {
-    this.payload.patientDelivery.etaMinutes = newValue;
-  }
-
-  get currentDeliveryStatus() {
-    return this.payload.patientDelivery.currentDeliveryStatus ?? null;
-  }
-
-  set currentDeliveryStatus(newValue) {
-    this.payload.patientDelivery.currentDeliveryStatus = newValue;
-  }
-
   get timestamps() {
     return this.payload.patientDelivery.timestamps ?? {};
   }
@@ -228,27 +224,25 @@ class Ringdown {
 
   // Form validation
 
-  static ascendingByOrder(a, b) {
-    if (a.order < b.order) {
-      return -1;
+  setValidationData(validationData) {
+    if (validationData) {
+      this.validationData = validationData;
+    } else {
+      this.validationData = validatedFields.reduce((result, field, i) => {
+        const [name, validator] = field;
+        const fieldValue = this[name];
+        const value = typeof validator === 'function'
+          ? validator(fieldValue)
+          : fieldValue;
+        const state = value
+          ? ValidationState.INPUT
+          : ValidationState.NO_INPUT;
+
+        result[name] = new PatientFieldData(name, i, state);
+
+        return result;
+      }, {});
     }
-    if (a.order === b.order) {
-      return 0;
-    }
-    return 1;
-  }
-
-  createValidationData(fields) {
-    return fields.reduce((result, field, i) => {
-      const [name, validator] = field;
-      const fieldValue = this[name];
-      const value = typeof validator === 'function' ? validator(fieldValue) : fieldValue;
-      const state = value ? ValidationState.INPUT : ValidationState.NO_INPUT;
-
-      result[name] = new PatientFieldData(name, i, state);
-
-      return result;
-    }, {});
   }
 
   validatePatientFields(updatedField, inputValue) {
@@ -295,7 +289,7 @@ Ringdown.propTypes = {
   dispatchCallNumber: PropTypes.number.isRequired,
   hospitalId: PropTypes.string.isRequired,
   // Patient Info
-  ...patient.getFieldHash(convertToPropType),
+  ...metadata.patient.getFieldHash(convertToPropType),
   // Status
   etaMinutes: PropTypes.number.isRequired,
   currentDeliveryStatus: PropTypes.oneOf(DeliveryStatus.ALL_STATUSES),
