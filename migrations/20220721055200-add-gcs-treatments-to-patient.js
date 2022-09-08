aeddf4acf714f170001beabfb14aa271b90a3a0f
Francis Li <mail@francisli.com>
2022-09-07 19:23:35 -0700

    Make migration a static standalone script

--- migrations/20220721055200-add-gcs-treatments-to-patient.js
+++ migrations/20220721055200-add-gcs-treatments-to-patient.js
@@ -1,29 +1,15 @@
-/* eslint-disable no-await-in-loop, no-restricted-syntax */
-const patient = require('../src/shared/metadata/patient');
-const convertToSequelizeField = require('../src/shared/convertToSequelizeField');
-
-const fields = patient.getFieldHash(convertToSequelizeField);
-const newFieldNames = ['treatmentNotes', 'glasgowComaScale'];
-const { tableName } = patient;
-
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      for (const name of newFieldNames) {
-        const { field, type } = fields[name];
-
-        await queryInterface.addColumn(tableName, field, type, { transaction });
-      }
+      await queryInterface.addColumn('patient', 'treatmentnotes', Sequelize.TEXT, { transaction });
+      await queryInterface.addColumn('patient', 'glasgowcomascale', Sequelize.INTEGER, { transaction });
     });
   },
 
   async down(queryInterface) {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      for (const name of newFieldNames) {
-        const { field } = fields[name];
-
-        await queryInterface.removeColumn(tableName, field, { transaction });
-      }
+      await queryInterface.removeColumn('patient', 'glasgowcomascale', { transaction });
+      await queryInterface.removeColumn('patient', 'treatmentnotes', { transaction });
     });
   },
 };
