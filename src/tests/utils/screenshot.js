6541bcf26392865d06a341264186969a0b4e0c22
John Dunning <fw@johndunning.com>
2022-09-11 14:07:02 -0700

    Add support for outputting numbered screenshots

--- src/tests/utils/screenshot.js
+++ src/tests/utils/screenshot.js
@@ -1,18 +1,29 @@
-class Screenshooter {
-  constructor({ page, path }) {
+import { resolve } from 'path';
+
+class ScreenShooter {
+  constructor({ page, outputDir = '', name = 'screenshot' }) {
     this.page = page;
-    this.path = path;
+    this.outputDir = outputDir;
+    this.name = name;
+    this.count = 0;
   }
 
-  screenshot(name) {
-  	return this.page.screenshot({ path: `${this.path}/${name}.png`, fullPage: true });
+  takeScreenshot() {
+    // eslint-disable-next-line no-plusplus
+    this.count++;
+
+    return this.page.screenshot({ path: this.getOutputFilePath(), fullPage: true });
+  }
+
+  getOutputFilePath() {
+    return resolve(this.outputDir, `${this.name}-${this.count}.png`);
   }
 }
 
-export function useScreenshot(options) {
-	const shooter = new Screenshooter(options);
+export default function useScreenshot(options) {
+  const shooter = new ScreenShooter(options);
 
   return function screenshot(...args) {
-  	return shooter.screenshot(...args);
-  }
+    return shooter.takeScreenshot(...args);
+  };
 }
