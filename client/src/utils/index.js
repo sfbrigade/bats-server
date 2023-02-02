553c97a0d2608eb84494065074919174775c1bd6
bryan kretz <kretzbryan@gmail.com>
2023-02-02 10:53:47 -0800

    remove log

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export function isValueEmpty(value) {
   const valueType = typeof value;
-  console.log(`valueType !== 'number' && valueType !== 'boolean' && !value`, valueType !== 'number' && valueType !== 'boolean' && !value);
   // count 0 and false as non-empty values
   return valueType !== 'number' && valueType !== 'boolean' && !value;
 }
