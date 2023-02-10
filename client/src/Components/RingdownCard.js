15c23d7eb41422f4cfdd6181b090cc7ae14df958
bryan kretz <kretzbryan@gmail.com>
2023-02-09 18:16:32 -0800

    handle arrived state ui in RingdownCard.js

--- client/src/Components/RingdownCard.js
+++ client/src/Components/RingdownCard.js
@@ -11,6 +11,7 @@ import RingdownDetails from './RingdownDetails';
 import Timestamp from './Timestamp';
 
 import './RingdownCard.scss';
+import { DateTime } from 'luxon';
 
 const { Status } = Ringdown;
 const AcknowledgedStatus = {
@@ -22,7 +23,7 @@ const AcknowledgedStatus = {
 function RingdownCard({ children, className, ringdown, dismissable, onStatusChange }) {
   const [isExpanded, setExpanded] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
-  const { currentDeliveryStatus, chiefComplaintDescription, etaDateTimeLocalObj } = ringdown;
+  const { currentDeliveryStatus, chiefComplaintDescription, etaDateTimeLocalObj, timestamps } = ringdown;
 
   function handleDismiss() {
     setShowConfirmation(false);
@@ -38,8 +39,22 @@ function RingdownCard({ children, className, ringdown, dismissable, onStatusChan
     currentDeliveryStatus === Status.OFFLOADED ? (
       <RingdownBadge status={currentDeliveryStatus} />
     ) : (
-      <Timestamp className="ringdown-card__status" label="ETA:" time={etaDateTimeLocalObj} />
+      <Timestamp
+        className="ringdown-card__status"
+        label={Status.ARRIVED ? 'Arrived At:' : 'ETA:'}
+        time={Status.ARRIVED ? DateTime.fromISO(timestamps.ARRIVED) : etaDateTimeLocalObj}
+      />
     );
+
+  // Below is a secondary option that may provide more clarity than the solution above if we reassign drawerTitle to be a let variable.
+  // switch(currentDeliveryStatus) {
+  //   case Status.OFFLOADED:
+  //     drawerTitle =  <RingdownBadge status={currentDeliveryStatus} />;
+  //   case Status.ARRIVED:
+  //     drawerTitle =  <Timestamp className="ringdown-card__status" label="Arrived At:" time={timestamps.ARRIVED} />
+  //   default:
+  //     drawerTitle =  <Timestamp className="ringdown-card__status" label="ETA:" time={etaDateTimeLocalObj} />
+  // }
 
   return (
     <div
