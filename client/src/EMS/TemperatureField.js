bc3a852d190d92cb6e4d5fd5ca0b534102ab62b7
bryan kretz <kretzbryan@gmail.com>
2023-03-01 17:55:32 -0800

    TemperatureField.js file created

---
+++ client/src/EMS/TemperatureField.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const TemperatureInput = () => {
+  const {data, onChange} = useForm()
+}
+
+const TemperatureField = () => {
+  return (
+    <div>TemperatureField</div>
+  )
+}
+
+export default TemperatureField
