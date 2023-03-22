dd75f4f0023b69d1bcf3da23577f6872f6244cbd
John Dunning <fw@johndunning.com>
2023-03-21 18:23:21 -0700

    Add package.json to shared
    In client code, import from `shared` package instead of a relative path.
    Remove craco.config.js and package, since importing code from outside /src is no longer necessary.
    Go back to using react-scripts directly.
    Refactor utils/.
    Fix typo in Ringdowns/RingdownsTable.js.

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
-export function isValueEmpty(value) {
-  const valueType = typeof value;
-  // count 0 and false as non-empty values
-  return valueType !== 'number' && valueType !== 'boolean' && !value;
-}
+export * from "./isValueEmpty";
+export * from "./convertToPropType";
