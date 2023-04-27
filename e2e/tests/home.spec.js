966b133e08e0f60e58ac77031902039bb44fbd9b
holliskuang <kuangholis@gmail.com>
2023-04-27 00:33:28 -0700

    Playwright Tests

--- e2e/tests/home.spec.js
+++ e2e/tests/home.spec.js
@@ -36,4 +36,30 @@ test.describe('home', () => {
     await password.press('Enter');
     await expect(page).toHaveURL('/er');
   });
+
+  test('shows the Routed logo and two factor authentication form', async ({ page }) => {
+    await page.goto('/');
+    await page.getByLabel('Email').fill(process.env.HOSPITAL_USER);
+    const password = page.getByLabel('Password');
+    await password.fill(process.env.HOSPITAL_PASS);
+    await password.press('Enter');
+    await expect(page).toHaveURL('/auth/local/twoFactor');
+    await expect(page).toHaveTitle(/Routed/);
+    await expect(page.getByAltText('Routed logo')).toBeVisible();
+    await expect(page.getByText('Please enter the Authoritzation Code that was sent to your E-mail')).toBeVisible();
+    await expect(page.getByLabel('Code')).toBeVisible();
+    await expect(page.getByText('Submit')).toBeVisible();
+  });
+
+  test('Shows Error after Incorrect Two Factor Authentication', async ({ page }) => {
+    await page.goto('/');
+    await page.getByLabel('Email').fill(process.env.HOSPITAL_USER);
+    const password = page.getByLabel('Password');
+    await password.fill(process.env.HOSPITAL_PASS);
+    await password.press('Enter');
+    await expect(page).toHaveURL('/auth/local/twoFactor');
+    await page.getByLabel('Code').fill('123456');
+    await page.getByText('Submit').click();
+    await expect(page.getByText('Invalid Authoritzation Code.')).toBeVisible();
+  });
 });
