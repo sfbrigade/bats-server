f948853e026434f9aa7c4321ac39691e66dd90b0
Oluwapelumi Fafowora <43530539+olupelumi@users.noreply.github.com>
2023-06-13 18:49:01 -0700

    Attempt sending api request when checking mfa enablement

--- client/src/Admin/Settings.js
+++ client/src/Admin/Settings.js
@@ -1,9 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
+
+import Context from '../Context';
+import ApiService from '../ApiService';
 
 function Settings() {
 
   // In the future, the initial value here will be determined by the result of an api request
   const [isMfaEnabled, setIsMfaEnabled] = useState(false);
+  const {organization } = useContext(Context);
+  console.log('organiz', organization)
+
+  const handleMfaToggle = () => {
+    // send the api request
+    // we also need the organization id
+        // Todo: fix the patch below. organization requests have worked in the hospital tab
+    ApiService.organizations.patch(organization.id, {isMfaEnabled: !isMfaEnabled}).then(response => {
+      console.log(response.data);
+      setIsMfaEnabled((curState) => !curState)
+
+    })
+  }
 
   return (
     <>
@@ -12,7 +28,7 @@ function Settings() {
             <h1>Settings</h1>
           </div>
           Mfa Toggle 
-          <input checked={isMfaEnabled} onChange={() => setIsMfaEnabled((curState) => !curState) } type='checkbox'></input>
+          <input checked={isMfaEnabled} onChange={handleMfaToggle} type='checkbox'></input>
         </main>
     </>
   );
