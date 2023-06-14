97286cffb855feed59cc44af267176825ab0868c
Oluwapelumi Fafowora <43530539+olupelumi@users.noreply.github.com>
2023-06-13 17:45:58 -0700

    Create initial checkbox

--- client/src/Admin/Settings.js
+++ client/src/Admin/Settings.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Settings() {
+
+  // In the future, the initial value here will be determined by the result of an api request
+  const [isMfaEnabled, setIsMfaEnabled] = useState(false);
+
   return (
     <>
         <main>
           <div className="display-flex flex-align-center flex-justify">
             <h1>Settings</h1>
           </div>
-          The mfa toggling will go here
+          Mfa Toggle 
+          <input checked={isMfaEnabled} onChange={() => setIsMfaEnabled((curState) => !curState) } type='checkbox'></input>
         </main>
     </>
   );
