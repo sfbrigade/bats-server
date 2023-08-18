96eb4f049cb0872fb64196c9966118b5510b0e1b
Vincent Shuali <100994924+canjalal@users.noreply.github.com>
2023-08-18 14:25:48 -0700

    [Closes #304] Write e2e tests for 2fa (#306)
    * Try opening mailcatcher
    
    * 2FA log in successful if run as standalone test
    
    * Add tests for successfully testing 2FA logins for EMS and Hospital, correct spelling
    
    * Run prettier
    
    * Update lock files to remove playwright-core
    
    * Fix dependency issues with @babel/plugin-proposal-private-property-in-object
    
    * Remove unnecessary import of chromium in twoFa.spec.js file
    
    * Initial mailcatcher circleCI commit
    
    * MailCatcher CircleCI trial config
    
    * Update CircleCI config with SMTP_HOST name as localhost
    
    * Remove run mailcatcher line in circleCI config
    
    * Move yarn build for e2e tests to package.json from playwright.config
    
    * For playwright tests, cd back into e2e after yarn build, and ALWAYS reuse server
    
    * Parameterize mailcatcher hostname in tests
    
    * Change mail URL based on NODE_ENV and delete messages after every 2FA test
    
    * Run prettier
    
    * Fix regex for seeing if bad auth code gotten, add 1 s delay before fetching code
    
    * Run prettier
    
    * Move to Playwright APIContext from fetch
    
    * Update seed to have 2FA for AMR organization
    
    ---------
    
    Co-authored-by: Anish Prasad <anishprasad@Anishs-MacBook-Pro.local>
    Co-authored-by: Francis Li <mail@francisli.com>

---
+++ e2e/tests/twoFA.spec.js
@@ -0,0 +1,118 @@
+const { test, expect } = require('@playwright/test');
+
+const SMTP_HOST = process.env.SMTP_HOST;
+
+test.beforeEach(async ({ request }) => {
+  await request.delete(`http://${SMTP_HOST}:1080/messages`);
+});
+
+test.describe('2FA', () => {
+  test.describe.configure({ mode: 'serial' });
+  test('shows the Routed logo and two factor authentication form', async ({ page }) => {
+    await page.goto('/');
+    await page.getByLabel('Email').fill('mission.bernal.er@c4sf.me');
+    const password = page.getByLabel('Password');
+    await password.fill('abcd1234');
+    await password.press('Enter');
+    await expect(page).toHaveURL('/auth/local/twoFactor');
+    await expect(page).toHaveTitle(/Routed/);
+    await expect(page.getByAltText('Routed logo')).toBeVisible();
+    await expect(page.getByText('Please enter the Authorization Code that was sent to your E-mail')).toBeVisible();
+    await expect(page.getByLabel('Code')).toBeVisible();
+    await expect(page.getByText('Submit')).toBeVisible();
+  });
+
+  test('Shows Error after Incorrect Two Factor Authentication', async ({ page }) => {
+    await page.goto('/');
+    await page.getByLabel('Email').fill('mission.bernal.er@c4sf.me');
+    const password = page.getByLabel('Password');
+    await password.fill('abcd1234');
+    await password.press('Enter');
+    await expect(page).toHaveURL('/auth/local/twoFactor');
+    await page.getByLabel('Code').fill('123456');
+    await page.getByText('Submit').click();
+    await expect(page.getByText('Invalid Authorization Code.')).toBeVisible();
+  });
+  test('logs in as EMS user with 2FA', async ({ context, request }) => {
+    const appPage = await context.newPage();
+
+    await appPage.goto('/');
+    await appPage.getByLabel('Email').fill('amr.user@c4sf.me');
+    const password = appPage.getByLabel('Password');
+    await password.fill('abcd1234');
+    await password.press('Enter');
+
+    await new Promise((resolve) => setTimeout(resolve, 200));
+
+    const allMsgsResponse = await request.get(`http://${SMTP_HOST}:1080/messages`);
+
+    const messages = await allMsgsResponse.json();
+
+    const lastMessageIdx = messages.length;
+
+    await expect(lastMessageIdx).toBeGreaterThan(0);
+
+    const msgResponse = await request.get(`http://${SMTP_HOST}:1080/messages/${lastMessageIdx}.plain`);
+
+    const emailText = await msgResponse.text();
+
+    // Use a regular expression to find "Code: " + six-digit number
+    const regex = /Code: (\d{6}) \./;
+    const foundCode = emailText.match(regex);
+
+    expect(foundCode).not.toBeNull();
+
+    const authCode = foundCode[1];
+
+    const code = appPage.getByLabel('Code');
+    await code.fill(authCode);
+    await code.press('Enter');
+
+    const pageBody = appPage.locator('body');
+    await expect(pageBody).not.toHaveText(/Invalid Authorization/);
+    await expect(appPage).toHaveURL('/ems');
+
+    // Gracefully close up everything
+    await context.close();
+  });
+  test('logs in as Hospital user with 2FA', async ({ context, request }) => {
+    const appPage = await context.newPage();
+
+    await appPage.goto('/');
+    await appPage.getByLabel('Email').fill('mission.bernal.er@c4sf.me');
+    const password = appPage.getByLabel('Password');
+    await password.fill('abcd1234');
+    await password.press('Enter');
+
+    await new Promise((resolve) => setTimeout(resolve, 200));
+
+    const allMsgsResponse = await request.get(`http://${SMTP_HOST}:1080/messages`);
+
+    const messages = await allMsgsResponse.json();
+
+    const lastMessageIdx = messages.length;
+
+    await expect(lastMessageIdx).toBeGreaterThan(0);
+
+    const msgResponse = await request.get(`http://${SMTP_HOST}:1080/messages/${lastMessageIdx}.plain`);
+
+    const emailText = await msgResponse.text();
+    // Use a regular expression to find "Code: " + six-digit number
+    const regex = /Code: (\d{6}) \./;
+    const foundCode = emailText.match(regex);
+
+    expect(foundCode).not.toBeNull();
+
+    const authCode = foundCode[1];
+    const code = appPage.getByLabel('Code');
+    await code.fill(authCode);
+    await code.press('Enter');
+
+    const pageBody = appPage.locator('body');
+    await expect(pageBody).not.toHaveText(/Invalid Authorization/);
+    await expect(appPage).toHaveURL('/er');
+
+    // Gracefully close up everything
+    await context.close();
+  });
+});
