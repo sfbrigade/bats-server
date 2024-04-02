d23e2890b18c8c71379f7d3a855c3df24d8d40b4
Francis Li <mail@francisli.com>
2024-04-02 11:25:55 -0700

    End MCI

--- client/src/Admin/Mcis/Mci.js
+++ client/src/Admin/Mcis/Mci.js
@@ -1,17 +1,33 @@
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import ApiService from '../../ApiService';
 import { DateTime } from 'luxon';
+
+import ApiService from '../../ApiService';
+import Alert from '../../Components/Alert';
 
 function Mci() {
   const { mciId } = useParams();
   const [data, setData] = useState();
+
+  const [confirmEnd, setConfirmEnd] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     if (mciId) {
       ApiService.mcis.get(mciId).then((response) => setData(response.data));
     }
   }, [mciId]);
+
+  async function onEnd() {
+    try {
+      const endedAt = DateTime.now().toISO();
+      await ApiService.mcis.update(mciId, { endedAt });
+      setData({ ...data, endedAt });
+      setConfirmEnd(false);
+    } catch (error) {
+      setError(error);
+    }
+  }
 
   return (
     <main>
@@ -20,20 +36,44 @@ function Mci() {
         <>
           {data.address1 && <div>{data.address1}</div>}
           {data.address2 && <div>{data.address2}</div>}
-          <div>
+          <div className="margin-bottom-1">
             <b>Started:</b> {DateTime.fromISO(data.startedAt).toLocaleString(DateTime.DATETIME_FULL)}
           </div>
           {data.endedAt && (
-            <div>
+            <div className="margin-bottom-1">
               <b>Ended:</b> {DateTime.fromISO(data.endedAt).toLocaleString(DateTime.DATETIME_FULL)}
             </div>
           )}
           <div className="usa-form-group">
-            {!data.endedAt && <button className="usa-button usa-button--secondary">End MCI</button>}
+            {!data.endedAt && (
+              <button onClick={() => setConfirmEnd(true)} className="usa-button usa-button--secondary">
+                End MCI
+              </button>
+            )}
             <Link to="edit" className="usa-button usa-button--primary">
               Edit
             </Link>
           </div>
+          {confirmEnd && (
+            <Alert
+              type="warning"
+              title="End MCI?"
+              message="Are you sure you wish to end this MCI?"
+              destructive="Yes"
+              cancel="No"
+              onDestructive={onEnd}
+              onCancel={() => setConfirmEnd(false)}
+            />
+          )}
+          {error && (
+            <Alert
+              type="error"
+              title="An unexpected error has occurred"
+              message={error.message}
+              cancel="OK"
+              onCancel={() => setError(null)}
+            />
+          )}
         </>
       )}
     </main>
