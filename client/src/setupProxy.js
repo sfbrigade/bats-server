48052771bbb5341f9a878db5c76ef67eab991ddb
Francis Li <mail@francisli.com>
2024-04-12 11:50:28 -0700

    Add oauth endpoints to proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware(['/api', '/auth', '/libraries', '/wss'], {
+    createProxyMiddleware(['/api', '/auth', '/libraries', '/oauth', '/wss'], {
       target: 'http://localhost:4000',
       changeOrigin: true,
       ws: true,
