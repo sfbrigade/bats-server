af51619fb7782016b5ecf07267c151bf477e45d4
Francis Li <mail@francisli.com>
2025-06-16 17:04:48 -0700

    Update Admin to show Hospital records for a VENUE Org

--- client/src/Admin/Hospitals/HospitalsList.js
+++ client/src/Admin/Hospitals/HospitalsList.js
@@ -6,50 +6,43 @@ import ApiService from '../../ApiService';
 function Hospitals() {
   const { user, organization } = useContext(Context);
   const navigate = useNavigate();
-  const [hospitals, setHospitals] = useState([]);
+  const [hospitals, setHospitals] = useState();
 
   useEffect(() => {
-    console.log('organization', organization);
     if (organization?.id) {
       ApiService.organizations.get(organization.id).then((response) => {
-        if (organization?.type === 'HEALTHCARE') {
-          setHospitals(response.data.hospitals);
-        }
+        setHospitals(response.data.hospitals);
       });
     }
   }, [organization]);
 
   return (
-    <>
-      {organization?.type === 'HEALTHCARE' && (
-        <main>
-          <div className="display-flex flex-align-center flex-justify">
-            <h1>Hospitals</h1>
-            {user?.isSuperUser && (
-              <Link to="new" className="usa-button usa-button--primary">
-                Add new Hospital
-              </Link>
-            )}
-          </div>
-          <table className="usa-table usa-table--striped usa-table--borderless usa-table--hoverable width-full">
-            <thead>
-              <tr>
-                <th className="w-50">Name</th>
-                <th>State Facility Code</th>
-              </tr>
-            </thead>
-            <tbody>
-              {hospitals?.map((h) => (
-                <tr key={h.id} onClick={() => navigate(h.id)}>
-                  <td>{h.name}</td>
-                  <td>{h.stateFacilityCode}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </main>
-      )}
-    </>
+    <main>
+      <div className="display-flex flex-align-center flex-justify">
+        <h1>Hospitals</h1>
+        {user?.isSuperUser && (
+          <Link to="new" className="usa-button usa-button--primary">
+            Add new Hospital
+          </Link>
+        )}
+      </div>
+      <table className="usa-table usa-table--striped usa-table--borderless usa-table--hoverable width-full">
+        <thead>
+          <tr>
+            <th className="w-50">Name</th>
+            <th>State Facility Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          {hospitals?.map((h) => (
+            <tr key={h.id} onClick={() => navigate(h.id)}>
+              <td>{h.name}</td>
+              <td>{h.stateFacilityCode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
   );
 }
 
